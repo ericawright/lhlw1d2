c0b97d6fdd211fd9561545cb80aa0e33fd46e714
Erica Wright <ericaewright@gmail.com>
2018-07-03 11:37:46 -0400

    July 3

--- add.js
+++ add.js
@@ -1,42 +1,52 @@
-// Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. if any arguement is not a whole number, skip it.
+// Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. if any argument is not a whole number, skip it.
 // Do support negative numbers
 
-// We used this code to do benchmarking testing a for loop vs a for...of loop. the foor loop won.
-// var longListOfNumbers = [];
-// for (i = 0; i < 99999; i++) {
-//   longListOfNumbers.push(1);
-// }
+
+// Get the time now for benchmarking purposes
 // var startTime = Date.now();
+// console.log(startTime);
 
-
-// break code into tiny reusable chunks
-function isWholeNumber(number) {
-  return (!isNaN(number) && number % 1 === 0);
-};
-
-// use a function so that it is reusable
-function sumNumbers(arrayOfNumbers) {
-  var sum = 0;
-  for (var i = 0; i < arrayOfNumbers.length; i++) {
-    var number = Number(arrayOfNumbers[i]);
-    if (isWholeNumber(number)) {
-      sum += number;
+function addNumbers(arrayOfNumbers) {
+  var tempsum = 0;
+  for( var i = 0; i < arrayOfNumbers.length; i++) {
+    // Turn argv strings into a number
+    var newNumber = Number(arrayOfNumbers[i]);
+    if (newNumber % 1 === 0) {
+      tempsum += newNumber;
     }
-  }
-  return sum;
-};
+  };
+  return tempsum;
+}
 
-var argvNumbers = process.argv.slice(2);
-console.log("The result is: ", sumNumbers(argvNumbers));
-console.log("The result is: ", sumNumbers([0, 10, "apple"]));
+// make sure to slice off the first two elements as they are references to node and the file locations
+var numbers = process.argv.slice(2);
 
-// console.log("time the program took", Date.now() - startTime)
+// for benchmarking use a large array instead of the argv entry
+// var numbers =  Array(100000000).fill(1);
+
+console.log("The result is: ", addNumbers(numbers)); // from argv
+console.log("The result is: ", addNumbers([1, 1])); // run again using different numbers
+
+// for benchmarking, subract the start time from the current time to see how much time has passed.
+// var difference = Date.now() - startTime;
+// console.log("the number of milliseconds: ", difference);
+
 
 // the for...of way of looping through the numbers
-// benefits, named variables are more readable vs number[i].
+// benefits, named variables are more readable vs arrayOfNumbers[i].
 // for (var number of arrayOfNumbers) {
 //   var realNumber = Number(number);
-//   if(!isNaN(realNumber) && realNumber % 1 === 0) {
+//   if(realNumber % 1 === 0) {
+//     sum += realNumber;
+//   }
+// }
+
+// the forEach way of looping through the numbers
+// pro: named variables are more readable vs arrayOfNumbers[i].
+// con: we benchmarked this, and it is slower than a for loop.
+// arrayOfNumbers.forEach(function(number){
+//   var realNumber = Number(number);
+//   if(realNumber % 1 === 0) {
 //     sum += realNumber;
 //   }
 // }
--- notes.md
+++ notes.md
@@ -1,17 +1,13 @@
 # The Dev Workflow
 
-- Approach as a puzzle
-- Break your problem down into small pieces
-- look up for syntax (MDN, stack overflow)
-- test what you've found (in the node repl or in your code, use lots of `console.log`)
-- What over How
-  - What is happening?
-  - What did you want to have happen?
-- always try to break your own code (entering unexpected inputs, users rarely behave)
-
-we want to write functions that make sense,  so
-   addTwoNumbersTogether(3) //bad
-doesn't make sense
+* Approach as a puzzle
+* Break your problem down into small pieces
+* look up for syntax (MDN, stack overflow)
+* test what you've found (in the node repl or in your code, use lots of console.log)
+* What over How:
+  - What is happening?
+  - What did you want to have happen?
+* always try to break your own code (entering unexpected inputs, as users rarely behave)
 
 Your code lies to you. The outcome tells you the truth. When you have an error, read the error. Use diagnostic tools a lot, like console.log
 
@@ -25,16 +21,16 @@ Your code lies to you. The outcome tells you the truth. When you have an error, 
 
 ## What we did
 We worked slowly through each step of creating the problem.
-1. how to get the inputs from the terminal (process.argv)
-2. how to remove the first two elements (.slice(2))
-3. how to turn the elements from strings to numbers (Number(element))
-4. how to how to check if the result is a real number or NaN (isNaN)
-5. adding the numbers together
-6. returning the numbers
-7. benchmarking (console.log and Date.now(), to test which type of loop was more efficient)
-8. refactoring (splitting into functions, make dry)
+* how to get the inputs from the terminal (process.argv)
+* how to remove the first two elements (.slice(2))
+* how to turn the elements from strings to numbers (Number(element))
+* how to how to check if the result is whole number (number % 1)
+* adding the numbers together
+* returning the numbers
+* benchmarking (console.log and Date.now(), to test which type of loop was more efficient)
+* refactoring (splitting into functions, make dry, add comments)
 
-For each problem you come across, search for the answer, test out what is suggested, and test it in your own code.
+For each problem you come across, search for the answer, test out what is suggested, and test it in your own code in small pieces. Make sure you test regularly after adding or changing anything, if you make a lot of changes all at once you might not know what is causing the error.
 
 Good luck in the rest of week 1!
 
